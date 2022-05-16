51e9f03c848609715a2efc24cf6cc73795b090a5
maciejw <maciejw@apr.pl>
2022-05-16 10:44:52 +0200

    Raw algorithm

--- test_battleship/Program.cs
+++ test_battleship/Program.cs
@@ -3,19 +3,18 @@ using System.Collections.Generic;
 
 public class Board
 {
-    public string[,] gameBoard = new string[10, 10]
+    public string[,] gameBoard = new string[10, 10];
+
+    public void createBoard()
     {
-      {"e", "e", "e", "e", "e", "e", "e", "e" ,"e", "e"},
-      {"e", "e", "e", "e", "e", "e", "e", "e" ,"e", "e"},
-      {"e", "e", "e", "e", "e", "e", "e", "e" ,"e", "e"},
-      {"e", "e", "e", "e", "e", "e", "e", "e" ,"e", "e"},
-      {"e", "e", "e", "e", "e", "e", "e", "e" ,"e", "e"},
-      {"e", "e", "e", "e", "e", "e", "e", "e" ,"e", "e"},
-      {"e", "e", "e", "e", "e", "e", "e", "e" ,"e", "e"},
-      {"e", "e", "e", "e", "e", "e", "e", "e" ,"e", "e"},
-      {"e", "e", "e", "e", "e", "e", "e", "e" ,"e", "e"},
-      {"e", "e", "e", "e", "e", "e", "e", "e" ,"e", "e"},
-    };
+        for (int i = 0; i < gameBoard.GetLength(0); i++)
+        {
+            for (int j = 0; j < gameBoard.GetLength(1); j++)
+            {
+                gameBoard[i,j] = "e";
+            }
+        }
+    }
 
     public void printBoard()
     {
@@ -44,7 +43,7 @@ public class Board
                         {
                             test = false;
                         }
-                    }            
+                    }
                 }
                 if (direction == 1)
                 {
@@ -130,25 +129,28 @@ public class Ship
         }
     }
 }
-public class Game 
-{ 
-    public bool checkShips(Ship c1, Ship b1, Ship d1, Ship s1, Ship p1)
+public class Game
+{
+    public bool checkShips(List<Ship> playerShips)
     {
-        if(c1.isDestroyed && b1.isDestroyed && d1.isDestroyed && s1.isDestroyed && p1.isDestroyed)
+        List<bool> destroyedShips = new();
+        foreach (var ship in playerShips)
         {
-            return false;
+            if (ship.isDestroyed)
+            {
+                destroyedShips.Add(true);
+            }
+            if (destroyedShips.Count == 5)
+            {
+                return false;
+            }
         }
         return true;
     }
-}
 
-class Program
-{
-    public static void Main(string[] args)
+    public List<Ship> createPlayerShips()
     {
-        Random random = new Random();
-        Game game = new();
-        List<Ship> playerOneShips = new List<Ship> {
+        List<Ship> playerShips = new List<Ship> {
             new("Carrier", "c", 5),
             new("Battleship", "b", 4),
             new("Destroyer", "d", 3),
@@ -156,14 +158,12 @@ class Program
             new("Patrol Boat", "p", 2),
         };
 
-        Ship c2 = new("Carrier", "c", 5);
-        Ship b2 = new("Battleship", "b", 4);
-        Ship d2 = new("Destroyer", "d", 3);
-        Ship s2 = new("Submarine", "s", 3);
-        Ship p2 = new("Patrol Boat", "p", 2);
-        Board board = new();
-        Board board2 = new();
-        int time = 0;
+        return playerShips;
+    }
+
+    public List<int> getRandomIntList()
+    {
+        Random random = new Random();
         List<int> listNumbers = new List<int>();
         int number;
         for (int i = 0; i < 6; i++)
@@ -174,8 +174,17 @@ class Program
             } while (listNumbers.Contains(number));
             listNumbers.Add(number);
         }
+
+        return listNumbers;
+    }
+
+    public void placeShipsOnBoard(Board board, List<Ship> playerShips)
+    {
+        Random random = new Random();
+        List<int> listNumbers = getRandomIntList();
+        int number;
         int index = 0;
-        foreach(var ship in playerOneShips)
+        foreach (var ship in playerShips)
         {
             do
             {
@@ -184,135 +193,100 @@ class Program
             board.placeShip(ship, 1, listNumbers[index], number);
             index++;
         }
+    }
 
-        listNumbers.Clear();
-        for (int i = 0; i < 6; i++)
+    public void playerShot(Board board, List<Ship> playerShips)
+    {
+        Random random = new Random();
+        int col;
+        int rows;
+        do
         {
-            do
+            col = random.Next(10);
+            rows = random.Next(10);
+        } while (board.gameBoard[col, rows] == "m" || board.gameBoard[col, rows] == "t");
+
+        foreach (var ship in playerShips)
+        {
+            if (board.gameBoard[col, rows] == ship.symbol)
             {
-                number = random.Next(10);
-            } while (listNumbers.Contains(number));
-            listNumbers.Add(number);
+                ship.hit();
+                board.gameBoard[col, rows] = "t";
+            }
         }
-        do
+        if (board.gameBoard[col, rows] == "t")
         {
-            number = random.Next(2);
-        } while (!board2.tryPlaceShip(c2, 1, listNumbers[0], number));
-        board2.placeShip(c2, 1, listNumbers[0], number);
-        do
+            board.gameBoard[col, rows] = "t";
+        }
+        else
         {
-            number = random.Next(2);
-        } while (!board2.tryPlaceShip(b2, 1, listNumbers[1], number));
-        board2.placeShip(b2, 1, listNumbers[1], number);
-        do
-        {
-            number = random.Next(2);
-        } while (!board2.tryPlaceShip(d2, 1, listNumbers[2], number));
-        board2.placeShip(d2, 1, listNumbers[2], number);
-        do
-        {
-            number = random.Next(2);
-        } while (!board2.tryPlaceShip(s2, 1, listNumbers[3], number));
-        board2.placeShip(s2, 1, listNumbers[3], number);
-        do
-        {
-            number = random.Next(2);
-        } while (!board2.tryPlaceShip(p2, 1, listNumbers[4], number));
-        board2.placeShip(p2, 1, listNumbers[4], number);
+            board.gameBoard[col, rows] = "m";
+            Console.WriteLine("Miss!");
+        }
+    }
 
-        Console.Clear();
-        board.printBoard();
-        Console.WriteLine("time: " + time);
-        board2.printBoard();
-
+    public void gameSimulation(Board board, Board board2, List<Ship> playerOneShips, List<Ship> playerTwoShips)
+    {
+        int time = 0;
         do
         {
+            Console.Clear();
+            board.printBoard();
+            Console.WriteLine("time: " + time);
+            board2.printBoard();
+
+            playerShot(board, playerOneShips);
+
+            System.Threading.Thread.Sleep(1000);
+            time++;
 
             Console.Clear();
             board.printBoard();
             Console.WriteLine("time: " + time);
             board2.printBoard();
 
-            int col = random.Next(10);
-            int rows = random.Next(10);
-            do
-            {
-                col = random.Next(10);
-                rows = random.Next(10);
-            } while (board.gameBoard[col, rows] == "m" || board.gameBoard[col, rows] == "t");
-
-            foreach (var ship in playerOneShips)
-            {
-                if (board.gameBoard[col, rows] == ship.symbol)
-                {
-                    ship.hit();
-                    board.gameBoard[col, rows] = "t";
-                }
-            }
-            if (board.gameBoard[col, rows] == "t")
-            {
-                board.gameBoard[col, rows] = "t";
-            }
-            else
-            {
-                board.gameBoard[col, rows] = "m";
-            }
+            playerShot(board2, playerTwoShips);
 
 
-            System.Threading.Thread.Sleep(100);
+            System.Threading.Thread.Sleep(1000);
             time++;
 
-            int col2 = random.Next(10);
-            int rows2 = random.Next(10);
-            do
-            {
-                col2 = random.Next(10);
-                rows2 = random.Next(10);
-            } while (board2.gameBoard[col2, rows2] == "m" || board2.gameBoard[col2, rows2] == "t");
-            if (board2.gameBoard[col2, rows2] == "c")
-            {
-                c2.hit();
-                board2.gameBoard[col2, rows2] = "t";
-            }
-            if (board2.gameBoard[col2, rows2] == "b")
-            {
-                b2.hit();
-                board2.gameBoard[col2, rows2] = "t";
-            }
-            if (board2.gameBoard[col2, rows2] == "d")
-            {
-                d2.hit();
-                board2.gameBoard[col2, rows2] = "t";
-            }
-            if (board2.gameBoard[col2, rows2] == "s")
-            {
-                s2.hit();
-                board2.gameBoard[col2, rows2] = "t";
-            }
-            if (board2.gameBoard[col2, rows2] == "p")
-            {
-                p2.hit();
-                board2.gameBoard[col2, rows2] = "t";
-            }
-            if (board2.gameBoard[col2, rows2] == "t")
-            {
-                board2.gameBoard[col2, rows2] = "t";
-            }
-            else
-            {
-                board2.gameBoard[col2, rows2] = "m";
-            }
-            System.Threading.Thread.Sleep(100);
-            time++;
-            if (!game.checkShips(playerOneShips[0], playerOneShips[1], playerOneShips[2], playerOneShips[3], playerOneShips[4]))
+
+            if (!checkShips(playerOneShips))
             {
                 Console.WriteLine("Player 2 Won!");
             }
-            if (!game.checkShips(c2, b2, d2, s2, p2))
+
+            if (!checkShips(playerTwoShips))
             {
                 Console.WriteLine("Player 1 Won!");
             }
-        } while (game.checkShips(playerOneShips[0], playerOneShips[1], playerOneShips[2], playerOneShips[3], playerOneShips[4]) && game.checkShips(c2, b2, d2, s2, p2));
+        } while (checkShips(playerOneShips) && checkShips(playerTwoShips));
     }
 }
-        
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Game game = new();
+        List<Ship> playerOneShips = game.createPlayerShips();
+
+        List<Ship> playerTwoShips = game.createPlayerShips();
+
+        Board board = new();
+        Board board2 = new();
+        board.createBoard();
+        board2.createBoard();
+        game.placeShipsOnBoard(board, playerOneShips);
+        game.placeShipsOnBoard(board2, playerTwoShips);
+ 
+
+        Console.Clear();
+        board.printBoard();
+        Console.WriteLine("time: 0" );
+        board2.printBoard();
+
+        game.gameSimulation(board, board2, playerOneShips, playerTwoShips);
+    }
+}
